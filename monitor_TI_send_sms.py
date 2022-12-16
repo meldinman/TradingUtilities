dc0450266bc108fc07b50992bf7c4713eda1806e
meldinman <77807315+meldinman@users.noreply.github.com>
2022-12-15 21:52:40 -0500

    Update monitor_TI_send_sms.py

--- monitor_TI_send_sms.py
+++ monitor_TI_send_sms.py
@@ -6,29 +6,29 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 def run_alert_log_blocking():
-    '''
+    """
     run scheduled function via blocking method
-    '''
+    """
     ALERTED = set()
     sched = BlockingScheduler()
     sched.add_job(send_alert_text, 'cron', day_of_week='mon-fri', hour='7-16', second='*/10',args=[ALERTED] )
     sched.start()
     
 def run_alert_log_background():
-    '''
+    """
     run scheduled function via background method
-    '''
+    """
     ALERTED = set()
     sched = BackgroundScheduler()
     sched.add_job(send_alert_text, 'cron', day_of_week='mon-fri', hour='7-16', second='*/10',args=[ALERTED] )
     sched.start()
     
 def send_alert_text(alerted):
-    '''
+    """
     get log file
     process new alerts
     send sms for new alerts
-    '''
+    """
     alert_log = get_todays_file()
     alerts = unique_alert(alerted, alert_log)
     for alert in alerts:
@@ -38,9 +38,9 @@ def send_alert_text(alerted):
         print("no new alerts found {}".format(datetime.now()))    
         
 def get_todays_file():
-    '''
+    """
     read csv if one has been created today
-    '''
+    """
     today = date.today()
     file_path = "alertlogging.Running Up.20221129.csv"
     try:
@@ -53,11 +53,11 @@ def get_todays_file():
 
         
 def unique_alert(alerted, alert_log):
-    '''
+    """
     compare alerts from incoming log with todays list of prior alerts
     add new alerts
     return update list of prior alerts
-    '''
+    """
     symbol_alerts = []
     if len(alert_log) > 0:
         firsts = alert_log.groupby('Symbol', as_index=False).first()
@@ -70,10 +70,10 @@ def unique_alert(alerted, alert_log):
     return symbol_alerts
         
 def reset_alerted_daily(alerted):
-    '''
+    """
     when running program overnight
     resets prior alert list each morning
-    '''
+    """
     now = datetime.now()
     if ( now.hour == 7 and now.minute == 1 and now.second == 1 ) :
         alerted = set()
